6731ac07f179352bb82c765f24dc85571dffaa48
gaiadobc <gaiadobici@gmail.com>
2020-12-11 15:21:22 +0100

    Aggiunto calcolo numero passeggeri per fasce orarie ed eseguito il merge tra i due dataframe di input

--- ESERCIZIO_VALERIO_DUE.py
+++ ESERCIZIO_VALERIO_DUE.py
@@ -11,18 +11,22 @@ import pandas as pd
 from numpy import var
 import argparse
 from datetime import datetime
+import numpy as np
 
 parser=argparse.ArgumentParser()
 
 parser.add_argument("-i", "--input_data", help="Complete path to the file containing yellow_tripdata",
-                    type=str, default='./yellow_tripdata_2020-02.csv')
+                    type=str, default='./dati/yellow_tripdata_2020-02.csv')
+
+parser.add_argument("-i2", "--input_data2", help="Complete path to the file containing taxi+_zone_lookup",
+                    type=str, default='./dati/taxi+_zone_lookup.csv')
 
 args=parser.parse_args()
 
 # salvo i dati su una struttura di tipo DataFrame 
 
 df= pd.read_csv(args.input_data)
-
+df1=pd.read_csv(args.input_data2)
 # Altro meteodo
 # df= pd.read_csv('./yellow_tripdata_2020.csv', nrows=5)
 
@@ -54,15 +58,31 @@ df['payment_type'].fillna(0, inplace=True)
 #     data=datetime.strptime(list(df.loc[vendor,'tpep_pickup_datetime'])[i], '%Y-%m-%d %H:%M:%S')
 #     df2[vendor,'Inizio Corsa'][i]=(datetime.timestamp(data))
 
-for i in range(400000):
+for i in range(1000):
     data=datetime.strptime(df['tpep_pickup_datetime'][i],'%Y-%m-%d %H:%M:%S')
-    data.replace(day=1)
-    df.loc[i,'Inizio Corsa']=datetime.timestamp(data)
+    data=data.replace(day=1,month=2)
+    df.loc[i,'Inizio Corsa']=(datetime.timestamp(data))
 
-# ora_interesse=  ('00:00:00')
-# ora_interesse=datetime.strptime(ora_interesse,'%H:%M:%S')
+#creato un unico dataframe
+df1 = df1.rename({'LocationID': 'PULocationID'}, axis=1)
+df_merged = pd.merge(df, df1, on=['PULocationID'])
+    
+#Individuo fasce orarie
+#se sottraggo due date mi restituisce la distanza in secondi
 
-# ora=datetime.timestamp(ora_interesse)
+ora_riferimento=datetime.timestamp(datetime.strptime('2020-02-01 00:00:00', '%Y-%m-%d %H:%M:%S'))
+fasce_orarie=np.array(range(0,25))*3600
+numero_passeggeri=[]
+for j in range(len(fasce_orarie)-1):
+    passeggeri=0
+    for i in range(1000):
+        
+        #tutte le corse che cadono dalle 00:00 alle 01:00
+        if fasce_orarie[j]<(abs(df['Inizio Corsa'][i]-ora_riferimento))<fasce_orarie[j+1]:
+            
+             passeggero=int(df['passenger_count'][i])
+             passeggeri+=passeggero
+    numero_passeggeri.append(passeggeri)
 
 
 
