c027449df42dec1684dd9bf2d14a82175697baa6
Francesca Ronci <francescaronci.11@gmail.com>
2020-12-17 13:58:10 +0100

    Eliminate ripetizioni dovute al merge

--- ESERCIZIO_VALERIO_DUE.py
+++ ESERCIZIO_VALERIO_DUE.py
@@ -38,31 +38,11 @@ while i in range(13):
 
          
 
-<<<<<<< HEAD
-# salvo i dati su una struttura di tipo DataFrame 
-
-=======
-parser=argparse.ArgumentParser()
-
-parser.add_argument("-i", "--input_data", help="Complete path to the file containing yellow_tripdata",
-                    type=str, default='./dati')
-
-
-# parser.add_argument("-i2", "--input_data2", help="Complete path to the file containing taxi+_zone_lookup",
-#                     type=str, default='./dati/taxi+_zone_lookup.csv')
-df1=pd.read_csv('taxi+_zone_lookup.csv')
->>>>>>> 7f17606aa271328235748fa1b96aa6952de1ffe0
 
 df1=pd.read_csv('taxi+_zone_lookup.csv')
 
 
-<<<<<<< HEAD
-=======
-df= pd.read_csv(args.input_data)
-#df1=pd.read_csv(args.input_data2)
-# Altro meteodo
-# df= pd.read_csv('./yellow_tripdata_2020.csv', nrows=5)
->>>>>>> 7f17606aa271328235748fa1b96aa6952de1ffe0
+
 
 # trasformo i NaN in 0 (farlo con un if)
 
@@ -77,7 +57,7 @@ df['RatecodeID'].fillna(0, inplace=True)
 df['store_and_fwd_flag'].fillna(0, inplace=True)
 
 df['payment_type'].fillna(0, inplace=True)
-#mettere un if sui nan
+
 
 # Creo nuova colonna con datatime trasformato in timestemp
 
@@ -101,27 +81,18 @@ da poterle poi confrontare con una data (anno-mese-giorno 00:00:00) di riferimen
 
 # usare apply
 
-=======
-
-'''
->>>>>>> 7f17606aa271328235748fa1b96aa6952de1ffe0
 for i in range(1000):
     data=datetime.strptime(df['tpep_pickup_datetime'][i],'%Y-%m-%d %H:%M:%S')
     data=data.replace(day=1,month=2,year=2020)
     df.loc[i,'Inizio Corsa']=(datetime.timestamp(data))
-    #trasforma in apply
+   
 
 #creato un unico dataframe che associa alla PULocationID il borough corrispondente
+
 df1 = df1.rename({'LocationID': 'PULocationID'}, axis=1)
-<<<<<<< HEAD
-df_merged = pd.merge(df, df1, on=['PULocationID'], how= 'left')
-    
-=======
 df_merged = pd.merge(df, df1, on=['PULocationID'],how='left')
     
 
-
->>>>>>> 7f17606aa271328235748fa1b96aa6952de1ffe0
 #imposto una data di riferimento
 ora_riferimento=datetime.timestamp(datetime.strptime('2020-02-01 00:00:00', '%Y-%m-%d %H:%M:%S'))
 
@@ -129,10 +100,7 @@ ora_riferimento=datetime.timestamp(datetime.strptime('2020-02-01 00:00:00', '%Y-
 fasce_orarie=np.array(range(0,25))*3600
 
 #Calcolo il numero di passeggeri per fascia oraria e li salvo in una lista
-<<<<<<< HEAD
 
-=======
->>>>>>> 7f17606aa271328235748fa1b96aa6952de1ffe0
 numero_passeggeri=[]
 for j in range(len(fasce_orarie)-1):
     passeggeri=0
