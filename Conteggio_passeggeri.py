b19e39bee1322aa7b3a64e5f4aa94f12f7affe43
gaiadobc <gaiadobici@gmail.com>
2020-12-22 18:41:41 +0100

    Aggiunti commenti sulle ultime modifiche della funzione Conteggio_passeggeri

--- Conteggio_passeggeri.py
+++ Conteggio_passeggeri.py
@@ -22,34 +22,32 @@ def count_passengers_hour(df_merged,boroughs,vettore_temporale):
     
     
     for borough in boroughs:
+        
         df_merged_b=df_merged.loc[(df_merged['Borough'] == borough)] #Estraggo dal DataFrame un sotto DataFrame relativo al borough considerato
           
-        #Calcolo il numero di passeggeri per fascia oraria e li salvo in una lista   
-        
-       # numero_passeggeri=[]
         fasce_orarie=[]
         
-        
         for j in range(len(vettore_temporale)-1):
-                
-                #passeggeri=0 # Inizializzo il numero di passeggeri con il valore zero
+            
+                #Creo il vettore delle fasce orarie
                 
                 fasce_orarie.append(str(vettore_temporale[j]/3600)+'0' + '-' +str(vettore_temporale[j+1]/3600)+'0') # Creo la lista contenente le fasce orarie 
                 
-                #for i in list(df_merged_b.index)[0:1000]: # Mi restringo ad un sotto DataFrame per velocizzare l'esecuzione
-                
                 '''
                     
-                Calcolo la differenza in seocondi tra la corsa i-esima e l'ora di riferimento. 
+                Calcolo la differenza in secondi tra la corsa i-esima e l'ora di riferimento. 
                 Se questo valore cade nell'intervallo definito da due elementi del vettore_temporale,
-                ovvero in una fascia oraria, aggiungo a passeggeri il numero di passeggeri di quella corsa
+                ovvero in una fascia oraria, aggiungo, nella colonna di df_passeggeri relativa al borough considerato,
+                la somma dei passeggeri relativa alla fascia oraria in esame.
                     
-               '''
-                    
+                '''
+                
+                #Mi restringo al sotto DataFrame relativo ad una specifica fascia oraria
+                
                 df_2=df_merged_b.loc[(df_merged_b['Inizio Corsa']-ora_riferimento>=vettore_temporale[j]) & (df_merged_b['Inizio Corsa']-ora_riferimento<vettore_temporale[j+1])]
                
                 
-                
+                #Inserisco nel DataFrame di output la somma dei passeggeri della singola fascia oraria
                         
                 df_passeggeri.loc[j,borough] =  df_2['passenger_count'].sum()      
 
